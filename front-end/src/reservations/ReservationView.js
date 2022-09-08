155676f7d3898a6d4dd961c38c828f5316a35bed
Long.H <longkr.work@gmail.com>
2022-09-07 20:25:39 -0500

    testing

--- front-end/src/reservations/ReservationView.js
+++ front-end/src/reservations/ReservationView.js
@@ -7,7 +7,6 @@ import { formatAsDate } from "../utils/date-time";
  */
 function ReservationView({ reservation, cancelHandler }) {
 
-    
     return (
         <div className="mb-1 pb-1 border-bottom">
             <div>  
@@ -15,11 +14,11 @@ function ReservationView({ reservation, cancelHandler }) {
                     <a className="mb-2 px-3 btn btn-outline-light"
                         href={`/reservations/${reservation.reservation_id}/seat`}
                     >
-                        seat
+                        Seat
                     </a>
                 }
                 <p data-reservation-id-status={`${reservation.reservation_id}`}>
-                    Status: {reservation.status.toUpperCase()}
+                    Status: {reservation.status}
                 </p> 
                 <p>Reservation ID: {reservation.reservation_id}</p>
                 <p>Fist Name: {reservation.first_name}</p>
@@ -29,14 +28,15 @@ function ReservationView({ reservation, cancelHandler }) {
                 <p>Date Reservation: {formatAsDate(reservation.reservation_date)}</p>
                 <p>Time Reservation: {reservation.reservation_time}</p>
                 <a  className="px-3 mr-3 mb-2 btn btn-outline-light"
-                    href={`/reservations/${reservation.reservation_id}/edit`}>
-                    edit
+                    href={`/reservations/${reservation.reservation_id}/edit`}
+                >
+                    Edit
                 </a>
                 <button data-reservation-id-cancel={reservation.reservation_id}
                     className="mb-2 btn btn-outline-light"                    
                     onClick={() => cancelHandler(reservation.reservation_id)}
                 >
-                    cancel
+                    Cancel
                 </button>
             </div>
         </div>
--- front-end/src/search/Search.js
+++ front-end/src/search/Search.js
@@ -10,43 +10,40 @@ import ReservationList from "../reservations/ReservationsList";
  *  return reservations user wants to find
  */
 function Search() {
-    const [phoneNumber, setPhoneNumer] = useState("");
-    const [reservations, setReservations] = useState([]);
-    const [error, setError] = useState(null);
+  const [phoneNumber, setPhoneNumer] = useState("");
+  const [reservations, setReservations] = useState([]);
+  const [error, setError] = useState(null);
 
-    const changeHandler = (e) => {
-        setPhoneNumer(e.target.value);
-    };
+  const changeHandler = (e) => {
+    setPhoneNumer(e.target.value);
+  };
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-        setError(null);
-        searchReservationsByPhone({ mobile_number: phoneNumber })
-            .then((data) => {
-                setReservations(data);
-                if(!data.length) {
-                    setError({ message: "No reservations found"})
-                }
-            })
-            .catch(setError)    
-    };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setError(null);
+    searchReservationsByPhone({ mobile_number: phoneNumber })
+      .then((data) => {
+        setReservations(data);
+        if (!data.length) {
+          setError({ message: "No reservations found" });
+        }
+      })
+      .catch(setError);
+  };
 
-    return (
-        <div>
-            <h1>Reservations</h1>
-            <h4>Search reservations by phone</h4>
-            <ErrorAlert error={error} />
-            <SearchForm phoneNumber={phoneNumber}
-                changeHandler={changeHandler}
-                submitHandler={submitHandler}
-            />
-            <ReservationList 
-                reservations={reservations}
-                setError={setError}
-            />
-        </div>
-    )
-};
+  return (
+    <div>
+      <h1>Reservations</h1>
+      <h4>Search reservations by phone</h4>
+      <ErrorAlert error={error} />
+      <SearchForm
+        phoneNumber={phoneNumber}
+        changeHandler={changeHandler}
+        submitHandler={submitHandler}
+      />
+      <ReservationList reservations={reservations} setError={setError} />
+    </div>
+  );
+}
 
 export default Search;
-
