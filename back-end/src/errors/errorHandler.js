795ca67fa83b5716f08c5fd658bfc7f2b21fc267
long-kr <longkr.work@gmail.com>
2025-08-03 23:10:38 -0500

    fix loading get reservation list error

--- back-end/src/errors/errorHandler.js
+++ back-end/src/errors/errorHandler.js
@@ -1,9 +1,9 @@
 /**
  * Express API error handler.
  */
-function errorHandler(error, request, response, next) {
-  const { status = 500, message = "Something went wrong!" } = error;
-  response.status(status).json({ error: message });
+function errorHandler(error, request, response) {
+	const { status = 500, message = "Something went wrong!" } = error;
+	response.status(status).json({ error: message });
 }
 
 module.exports = errorHandler;
