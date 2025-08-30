abe7c21989d6f9423e12605fc8cbd116065157bb
long-kr <longkr.work@gmail.com>
2025-08-29 23:39:23 -0500

    add executablePath

--- front-end/jest-puppeteer.config.js
+++ front-end/jest-puppeteer.config.js
@@ -4,5 +4,6 @@ module.exports = {
 		args: ["--no-sandbox", "--disable-setuid-sandbox"],
 		slowMo: process.env.SLO_MO || 0,
 		devtools: true,
+		executablePath: "/usr/bin/chromium-browser",
 	},
 };
