97bead5672b96d65038471f66c7cca7852fbd061
long-kr <longkr.work@gmail.com>
2025-08-29 23:57:16 -0500

    try puppeteer container

--- front-end/jest-puppeteer.config.js
+++ front-end/jest-puppeteer.config.js
@@ -1,9 +1,6 @@
 module.exports = {
 	launch: {
 		headless: (process.env.HEADLESS || "true") === "true",
-		args: ["--no-sandbox", "--disable-setuid-sandbox"],
 		slowMo: process.env.SLO_MO || 0,
-		devtools: true,
-		executablePath: "/usr/bin/chromium-browser",
 	},
 };
