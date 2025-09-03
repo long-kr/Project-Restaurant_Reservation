0253796e55c777096a71d4f3107c166b672898cf
long-kr <longkr.work@gmail.com>
2025-09-03 12:06:36 -0500

    change API_BASE_URL to fit react 18

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -7,7 +7,8 @@ import {
 	default as formatReservationTime,
 } from "./format-reservation-date";
 
-const API_BASE_URL = process.env.BASE_URL || "http://localhost:5001";
+const API_BASE_URL =
+	process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
